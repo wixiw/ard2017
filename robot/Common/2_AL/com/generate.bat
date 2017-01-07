a96aee9cbbc2652be03fdd017afed8d220c9c95c
Willy <lambert.willy@gmail.com>
2017-01-07 03:06:09 +0100

    Protobuf layer added for Teleop. Vizu is now capable to start a match

--- robot/Common/2_AL/com/generate.bat
+++
@@ -1,4 +0,0 @@
-echo "8==>"
-set PROTOC="C:\Users\wix\Documents\GitHub\ard2017\devenv\nanopb-0.3.7-windows-x86\generator-bin\protoc.exe"
-call %PROTOC% --nanopb_out=%1 --proto_path=%1 %1\Teleop.proto
-pause
