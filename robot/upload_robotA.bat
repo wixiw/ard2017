6635b721e6a3e5888ca2d7f1e6d79699d51e593a
Willy <lambert.willy@gmail.com>
2017-03-21 22:54:51 +0100

    Add bossac to repo

--- robot/upload_robotA.bat
+++ robot/upload_robotA.bat
@@ -1,3 +1,3 @@
-SET BOSSAC=%USERPROFILE%\AppData\Local\Arduino15\packages\arduino\tools\bossac\1.6.1-arduino\bossac.exe 
+SET BOSSAC=bossac.exe 
 SET FILE=RobotA\Debug\RobotA.bin
 BossacArduinoDue.bat %BOSSAC% %FILE%
