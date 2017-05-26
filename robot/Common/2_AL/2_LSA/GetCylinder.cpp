086ce439ecd562a925a4e2e27798bc59d87e0e53
Mougna <tanguy.espinasse@gmail.com>
2017-05-26 03:56:48 +0200

    Get C as a dispenser = win

--- robot/Common/2_AL/2_LSA/GetCylinder.cpp
+++ robot/Common/2_AL/2_LSA/GetCylinder.cpp
@@ -44,7 +44,7 @@ void LSA_GetCylinder_B::goToEntryPoint()
  */
 
 LSA_GetCylinder_C::LSA_GetCylinder_C(Robot2017& robot, eLSA_GetCylinder_Recal type):
-        LSA2017(robot, "LSAPooMiddle", Pose2D(780, -600, -108))
+        LSA2017(robot, "LSAPooMiddle", Pose2D(715, -640, -90))
 {
     fsm.setTimer(&(robot.lifecycle.fsmTimer));
     fsm.setDefaultSCI_OCB(this);
